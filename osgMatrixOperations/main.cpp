72abb318941c65b288e5ecc56e542507a2931808
Aoi <fancyyard@163.com>
2018-07-06 17:14:50 +0800

    创建矩阵操作测试项目

---
+++ osgMatrixOperations/main.cpp
@@ -0,0 +1,14 @@
+#include"../OSGStudyProject/comdepen.h"
+#ifdef _DEBUG
+#pragma comment(lib,"../x64/Debug/osgAddDepen.lib")
+#else
+#pragma comment(lib,"../x64/Release/osgAddDepen.lib")
+#endif // _DEBUG
+
+#include<osgViewer/Viewer>
+#include<osgDB/ReadFile>
+
+int main() {
+
+	return 0;
+}
